42bbc00cd47646b39950fa392abd8c1c35acc8dd
Dell <1529146935@qq.com>
2022-04-06 15:14:02 +0800

    2022-04-06 15:00:00  SET UP VIEW

---
+++ main/src/main/webapp/mock/mockdb.js
@@ -0,0 +1,18 @@
+const mockUsers = [
+  { id: 1, username: '洛必达', password: '123456', phone: '13770366999', createTime: '2020-05-11 20:00:00'},
+  { id: 2, username: '莱布尼茨', password: '123456', phone: '13770366999', createTime: '2020-09-11 20:00:00'},
+  { id: 3, username: '牛顿', password: '123456', phone: '13770366999', createTime: '2020-07-11 20:00:00'},
+  { id: 4, username: '特朗普', password: '654321', phone: '13770366999', createTime: '2020-01-11 20:00:00'},
+  { id: 5, username: '普京', password: '852369', phone: '13770366999', createTime: '2020-01-11 20:00:00'},
+  { id: 6, username: '洛必达', password: '123456', phone: '13770366999', createTime: '2020-05-11 20:00:00'},
+  { id: 7, username: '莱布尼茨', password: '123456', phone: '13770366999', createTime: '2020-09-11 20:00:00'},
+  { id: 8, username: '牛顿', password: '123456', phone: '13770366999', createTime: '2020-07-11 20:00:00'},
+  { id: 9, username: '特朗普', password: '654321', phone: '13770366999', createTime: '2020-01-11 20:00:00'},
+  { id: 10, username: '普京', password: '852369', phone: '13770366999', createTime: '2020-01-11 20:00:00'},
+  { id: 11, username: '洛必达', password: '123456', phone: '13770366999', createTime: '2020-05-11 20:00:00'},
+  { id: 12, username: '莱布尼茨', password: '123456', phone: '13770366999', createTime: '2020-09-11 20:00:00'},
+  { id: 13, username: '牛顿', password: '123456', phone: '13770366999', createTime: '2020-07-11 20:00:00'},
+  { id: 14, username: '特朗普', password: '654321', phone: '13770366999', createTime: '2020-01-11 20:00:00'},
+]
+
+export default mockUsers
